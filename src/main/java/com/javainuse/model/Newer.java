4f66ba0f9baf257e832b20bf88ea5aad868dad78
user <user@user-PC>
2018-09-25 16:34:54 +0530

    here

--- src/main/java/com/javainuse/model/Newer.java
+++ src/main/java/com/javainuse/model/Newer.java
@@ -3,5 +3,17 @@ package com.javainuse.model;
 public class Newer {
 	private String Hello;
 	private String hell;
+	public String getHell() {
+		return hell;
+	}
+	public void setHell(String hell) {
+		this.hell = hell;
+	}
+	public String getLatest() {
+		return Latest;
+	}
+	public void setLatest(String latest) {
+		Latest = latest;
+	}
 	private String Latest ;
 }
