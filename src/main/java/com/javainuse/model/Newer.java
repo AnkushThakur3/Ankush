bb83f0b8b3d36aaeade3194deeb04acf5b913b98
user <user@user-PC>
2018-09-25 16:14:04 +0530

    make setter getter

--- src/main/java/com/javainuse/model/Newer.java
+++ src/main/java/com/javainuse/model/Newer.java
@@ -1,6 +1,24 @@
 package com.javainuse.model;
 
 public class Newer {
+	public String getHello() {
+		return Hello;
+	}
+	public void setHello(String hello) {
+		Hello = hello;
+	}
+	public String getHell() {
+		return hell;
+	}
+	public void setHell(String hell) {
+		this.hell = hell;
+	}
+	public String getLatest() {
+		return Latest;
+	}
+	public void setLatest(String latest) {
+		Latest = latest;
+	}
 	private String Hello;
 	private String hell;
 	private String Latest ;
